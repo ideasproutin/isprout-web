c65015c129936c976ef900fd382c0e6d8f6e404e
Deva <madamshatidevanandh@gmail.com>
2026-01-30 15:07:34 +0530

    Refactor AboutUs component for improved formatting and consistency

--- src/pages/aboutus/aboutus.tsx
+++ src/pages/aboutus/aboutus.tsx
@@ -6,28 +6,27 @@ import MissionAndVision from "./missionandvision";
 import Visionaries from "../home/components/visionaries";
 import Evolution from "./evolution";
 import BlogsNews from "../home/components/blogs_news";
-// import FutureOfWork from "../home/components/futureofwork";
-import YouTubeVideo from "../home/components/youtubevideo";
+import FutureOfWork from "../home/components/futureofwork";
+//import YouTubeVideo from "../home/components/youtubevideo";
 import Footer from "../../components/footer/footer";
 import ScrollToTop from "../../components/ScrollToTop/ScrollToTop";
 import { COLORS } from "../../helpers/constants/Colors";
 
 const AboutUs: React.FC = () => {
-	return (
-		<div className='min-h-screen' style={{ backgroundColor: COLORS.white }}>
-			<IntroSection />
-			<WhoWeAre />
-			<Innovators />
-			<MissionAndVision />
-			<Visionaries />
-			<Evolution />
-			<BlogsNews />
-			{/* <FutureOfWork /> */}
-			<YouTubeVideo />
-			<Footer />
-			<ScrollToTop />
-		</div>
-	);
+  return (
+    <div className="min-h-screen" style={{ backgroundColor: COLORS.white }}>
+      <IntroSection />
+      <WhoWeAre />
+      <Innovators />
+      <MissionAndVision />
+      <Visionaries />
+      <Evolution />
+      <BlogsNews />
+      <FutureOfWork />
+      <Footer />
+      <ScrollToTop />
+    </div>
+  );
 };
 
 export default AboutUs;
