b72625d9fdde2c2a56c210d577ed042e01e79aa6
Deva <madamshatidevanandh@gmail.com>
2026-01-30 17:16:41 +0530

    Uncomment YouTubeVideo import in AboutUs component and comment out FutureOfWork section in render

--- src/pages/aboutus/aboutus.tsx
+++ src/pages/aboutus/aboutus.tsx
@@ -7,7 +7,7 @@ import Visionaries from "../home/components/visionaries";
 import Evolution from "./evolution";
 import BlogsNews from "../home/components/blogs_news";
 import FutureOfWork from "../home/components/futureofwork";
-//import YouTubeVideo from "../home/components/youtubevideo";
+import YouTubeVideo from "../home/components/youtubevideo";
 import Footer from "../../components/footer/footer";
 import ScrollToTop from "../../components/ScrollToTop/ScrollToTop";
 import { COLORS } from "../../helpers/constants/Colors";
@@ -22,7 +22,8 @@ const AboutUs: React.FC = () => {
       <Visionaries />
       <Evolution />
       <BlogsNews />
-      <FutureOfWork />
+      {/* <FutureOfWork /> */}
+      <YouTubeVideo />
       <Footer />
       <ScrollToTop />
     </div>
